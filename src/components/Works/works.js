5cce4b1be25ed88f307dfae0640d772447f3999a
Alessandro Velasco <velasco.alessandro@gmail.com>
2024-01-17 16:32:35 +0100

    adding a project

---
+++ src/components/Works/works.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './works.css';
+import Portfolio1 from '../../assets/portfolio-1.png'
+import Portfolio2 from '../../assets/portfolio-2.png'
+import Portfolio3 from '../../assets/portfolio-3.png'
+
+
+const Works = () => {
+  return (
+    <section id='works'>
+        <h2 className='workstitle'>My Portfolio</h2>
+        <span className='worksDesc'></span>
+        <div className='worksImgs'>
+            <img src={Portfolio1} alt='' className='worksImg' />
+            <img src={Portfolio2} alt='' className='worksImg' />
+            <img src={Portfolio3} alt='' className='worksImg' />
+             </div>
+             <button className='workBtn'>See More</button>
+    </section>
+  )
+}
+
+export default Works;
