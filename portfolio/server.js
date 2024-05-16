2a928b48bca1d13885951cde3f530a5f5d7ef8db
Alessandro Velasco <108097725+Alessandro-Velasco@users.noreply.github.com>
2024-05-16 16:08:35 +0200

    add cv to button Hire me

---
+++ portfolio/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Define la carpeta donde se encuentran los archivos estáticos (por ejemplo, PDF)
+const publicPath = path.join(__dirname, 'public');
+
+// Configura Express para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(publicPath));
+
+// Inicia el servidor en el puerto 3000 (o el puerto que desees)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
+});
--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './intro.css'
 import bg from '../../assets/image.png';
 import btnImg from '../../assets/hireme.png';
-import { Link } from 'react-scroll';
+
 
 
 const Intro = () => {
@@ -10,9 +10,14 @@ const Intro = () => {
     <section id="intro">
       <div className='introContent'>
        <span className='hello'>Hello,</span>
-       <span className='introText'>I'm <span className='introName'>Velasco</span> <br />Front-End Developer</span>
+       <span className='introText'>I'm <span className='introName'>Alessandro</span> <br />Front-End Developer</span>
        <p className='introPara'>I am a skilled Front-End Developer with experience in creating <br />visually and user friendly websites. </p>
-       <Link><button className='btn'><img src={btnImg} alt='Hire me' className='btnImg'></img>Hire me</button></Link>
+       <a href="/velasco-alessandro-cv.pdf.pdf" download>
+          <button className='btn'>
+            <img src={btnImg} alt='Hire me' className='btnImg' />
+            Hire me
+          </button>
+        </a>
       </div>
       <img src={bg} alt="Profile" className="bg" />
     </section>
