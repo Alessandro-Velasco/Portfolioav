843e665f83b4e78da665da839419bb28eecd1631
Alessandro Velasco <108097725+Alessandro-Velasco@users.noreply.github.com>
2024-05-21 10:29:18 +0200

    add cv dowload to hire me button

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './intro.css'
 import bg from '../../assets/image.png';
 import btnImg from '../../assets/hireme.png';
-
+import { Link } from 'react-scroll';
 
 
 const Intro = () => {
@@ -10,14 +10,9 @@ const Intro = () => {
     <section id="intro">
       <div className='introContent'>
        <span className='hello'>Hello,</span>
-       <span className='introText'>I'm <span className='introName'>Alessandro</span> <br />Front-End Developer</span>
+       <span className='introText'>I'm <span className='introName'>Velasco</span> <br />Front-End Developer</span>
        <p className='introPara'>I am a skilled Front-End Developer with experience in creating <br />visually and user friendly websites. </p>
-       <a href="/velasco-alessandro-cv.pdf.pdf" download>
-          <button className='btn'>
-            <img src={btnImg} alt='Hire me' className='btnImg' />
-            Hire me
-          </button>
-        </a>
+       <Link><button className='btn'><img src={btnImg} alt='Hire me' className='btnImg'></img>Hire me</button></Link>
       </div>
       <img src={bg} alt="Profile" className="bg" />
     </section>
--- portfolio/src/components/Works/works.js
+++ portfolio/src/components/Works/works.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './works.css';
-import Portfolio1 from '../../assets/portfolio-1.PNG'
+import Portfolio1 from '../../assets/portfolio-1.png'
 import Portfolio2 from '../../assets/portfolio-2.png'
 import Portfolio3 from '../../assets/portfolio-3.png'
 
@@ -11,9 +11,7 @@ const Works = () => {
         <h2 className='workstitle'>My Portfolio</h2>
         <span className='worksDesc'></span>
         <div className='worksImgs'>
-            <a href="https://mern-estate-xdt0.onrender.com/">
             <img src={Portfolio1} alt='' className='worksImg' />
-            </a>
             <img src={Portfolio2} alt='' className='worksImg' />
             <img src={Portfolio3} alt='' className='worksImg' />
              </div>
